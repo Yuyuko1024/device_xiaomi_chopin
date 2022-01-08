8114c6630cc0da1103daf184c3fa1fca36d20f4b
GoogleChinaCEO <dingwenxuan4@gmail.com>
2022-01-08 11:44:29 +0800

    chopin: Add Custom Light Hal

--- device.mk
+++ device.mk
@@ -58,6 +58,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.chopin
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
