32497587dd698618cfcf1de87602825dc33e73cb
ManshuTyagi <himanshut567@gmail.com>
2022-01-08 13:27:20 +0800

    chopin: Add debug IMS props to workaround CarrierConfig restrictions We support IMS features on carriers not listed in the CarrierConfig, so let's use the props to enable it everywhere.

--- system_prop.mk
+++ system_prop.mk
@@ -3,3 +3,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
 	ro.mtk_perf_fast_start_win=1 \
 	ro.mtk_perf_response_time=1
+
+# IMS
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1
