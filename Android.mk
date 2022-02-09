8c82a5080da06473c1b5a2a1648ae1bf601fa5a4
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-02-09 02:55:52 +0000

    chopin: fix typo error i'm a fool

--- Android.mk
+++ Android.mk
@@ -18,4 +18,3 @@ LOCAL_PATH := $(call my-dir)
 
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
-endif
