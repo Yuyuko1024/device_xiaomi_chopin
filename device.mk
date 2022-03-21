86922477b1d84cb7662fd676b829121ca806cf32
erfanoabdi <erfangplus@gmail.com>
2022-03-21 02:49:49 +0000

    chopin: Include mtk ims stack

--- device.mk
+++ device.mk
@@ -144,6 +144,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
