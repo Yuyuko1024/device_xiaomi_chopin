435051a718aed4d1d16aa823ce3d6a4bfa9eb6b5
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-03-21 00:34:19 +0000

    chopin: Build libhidltransport

--- device.mk
+++ device.mk
@@ -134,7 +134,8 @@ PRODUCT_COPY_FILES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport
 
 # IFAA manager
 PRODUCT_PACKAGES += \
