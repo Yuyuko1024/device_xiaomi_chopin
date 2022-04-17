f420260766e380ee82f8fc3a35f8627233026627
PeterCxy <peter@typeblog.net>
2022-04-17 06:06:03 +0000

    chopin:Add fillter for chopin

--- Android.mk
+++ Android.mk
@@ -16,5 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(TARGET_DEVICE),chopin)
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+endif
